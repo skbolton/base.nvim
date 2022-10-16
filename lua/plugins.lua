4506f7485a6fe054efab0e5607555bda1025381f
Stephen Bolton <stephen@bitsonthemind.com>
2022-10-16 06:40:05 -0400

    Add dependency of telescope

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -31,6 +31,7 @@ return require('packer').startup(function(use)
 
 	use {
     'nvim-telescope/telescope.nvim',
+    requires = {'nvim-lua/plenary.nvim'},
 		config = function()
       require('fuzzy')
 		end
